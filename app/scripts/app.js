05aca40e1bfd6901a0f4814055c5c54040848de8
Vitaliy Lazarenko <norfild@gmail.com>
2015-09-15 18:08:26 +0300

    change btn login

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -217,6 +217,7 @@
     .run(['$rootScope', '$location', '$cookieStore', '$http', function ($rootScope, $location, $cookieStore, $http) {
         // keep user logged in after page refresh
         $rootScope.globals = $cookieStore.get('globals') || {};
+
         if ($rootScope.globals.currentUser) {
             $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
         }
